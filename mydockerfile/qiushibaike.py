7f9f6a0948b6ef4118e029df9f27009a4fbcb3aa
DESKTOP-16G2IT2\lenovo <liang.yao@silknets.com>
2019-01-25 18:29:30 +0800

    add mydockerfile

---
+++ mydockerfile/qiushibaike.py
@@ -0,0 +1,31 @@
+
+from flask import Flask,request,jsonify
+import time
+import requests
+from bs4 import BeautifulSoup
+app = Flask(__name__)
+app.config['SECRET_KEY']='you-never-know'
+
+
+def crawl():
+    headers = {
+            'cache-control': "no-cache",
+	            'postman-token': "dd6b12bd-efbf-532a-475e-6568214bd3cb",
+		            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+			        }
+    url = 'https://www.qiushibaike.com/text/'
+    res=requests.get(url=url,headers=headers).text
+    soup = BeautifulSoup(res,'lxml')
+    text=''
+    for i in soup.find_all('div',class_='content'):
+        text=text+i.text
+    return text
+
+
+@app.route('/qiubai',methods=['GET'])
+def QIUBAI():
+    content=crawl()
+    return content
+if __name__ == '__main__':
+    app.run(host=('0.0.0.0'),port=5000,debug=True)
+
